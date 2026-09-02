5b77fe7aa2127c260a21baaa55f540576aaf864e
md5md4w2hc-commits <md5md4w2hc@privaterelay.appleid.com>
2026-09-02 19:48:09 +0530

    starting

--- teen patti/main.cpp
+++ teen patti/main.cpp
@@ -1,14 +1,13 @@
-//
-//  main.cpp
-//  teen patti
-//
-//  Created by Pranjal Srivastava on 02/09/26.
-//
-
 #include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return EXIT_SUCCESS;
+int main()
+{
+    std::cout << "hello";
+    
+    
+    std::cin.get();
 }
+
