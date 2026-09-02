df65b854711bbbb6b12527bd2590a56db53ad76a
md5md4w2hc-commits <md5md4w2hc@privaterelay.appleid.com>
2026-09-02 13:26:18 +0530

    Initial Commit

---
+++ teen patti/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  teen patti
+//
+//  Created by Pranjal Srivastava on 02/09/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
