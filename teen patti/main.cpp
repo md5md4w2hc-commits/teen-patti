900a85b17078eed94129e27b26c7fa0a87ac1499
md5md4w2hc-commits <md5md4w2hc@privaterelay.appleid.com>
2026-09-05 15:23:16 +0530

    re creating

--- teen patti/main.cpp
+++ teen patti/main.cpp
@@ -3,6 +3,8 @@
 #include <random>
 #include <numeric>
 #include <array>
+#include <algorithm>
+#include <tuple>
 
 struct Card
 {
@@ -17,38 +19,52 @@ struct Hand
     Card card3;
 };
 
-int Random()
+struct Player
+{
+    Hand hand;
+};
+
+int Random(int max)
 {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dist(0, 51);
+    std::uniform_int_distribution<> dist(0, max - 1);
     int r = dist(gen);
     
     return r;
 }
 
-std::vector<Card> Shuffel(std::vector<Card> deck , int r)
+std::vector<Card> Shuffel(std::vector<Card>& deck, int r)
 {
-    std::erase(deck, r);
-    
+    deck.erase(deck.begin() + r);
     return deck;
+}
+
+std::tuple<Card , std::vector<Card>> Drawing(std::vector<Card> deck)
+{
+    int r = Random(deck.size());
+    Card x1 = deck.at(r);
+    deck = Shuffel(deck , r);
+    return {x1 , deck};
 }
 
 int main()
 {
-    std::vector<Card> deck = {
+    std::vector<Card> deck =
+    {
         {1,'H'},{2,'H'},{3,'H'},{4,'H'},{5,'H'},{6,'H'},{7,'H'},{8,'H'},{9,'H'},{10,'H'},{11,'H'},{12,'H'},{13,'H'},
         {1,'D'},{2,'D'},{3,'D'},{4,'D'},{5,'D'},{6,'D'},{7,'D'},{8,'D'},{9,'D'},{10,'D'},{11,'D'},{12,'D'},{13,'D'},
         {1,'C'},{2,'C'},{3,'C'},{4,'C'},{5,'C'},{6,'C'},{7,'C'},{8,'C'},{9,'C'},{10,'C'},{11,'C'},{12,'C'},{13,'C'},
         {1,'S'},{2,'S'},{3,'S'},{4,'S'},{5,'S'},{6,'S'},{7,'S'},{8,'S'},{9,'S'},{10,'S'},{11,'S'},{12,'S'},{13,'S'}
     };
-
-    int r = Random();
     
-    Card x = deck.at(r);
+    auto[x1 , deck_shuffeld] = Drawing(deck);
     
+    std::cout << x1.rank << x1.suit ;
     
     std::vector<Hand> player ;
+    
+    player = {{}};
     
     std::cin.get();
 }
